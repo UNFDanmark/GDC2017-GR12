c10d97276c358e11d968ccf37091a8cfd7115be7
Programmering <johan.thomsen@hotmail.com>
2017-07-24 19:59:21 +0200

    Objective spawners er indsat, mangler scripts

---
+++ MazeRunner2k17/Assets/Scripts/ObjectActivator.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectActivator : MonoBehaviour {
+
+    public int[] spawnDeciders = new int[3];
+
+
+	// Use this for initialization
+	void Start () {
+        for (int i = 0; 3 > i; i++)
+        {
+            spawnDeciders[i] = Random.Range(7 * i, 7 * (i + 1));
+            spawnObjective("Object_" + spawnDeciders[i]);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
