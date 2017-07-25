faf4fcd7d2f4e088243f97722c6bd6306f7793aa
Programmering <johan.thomsen@hotmail.com>
2017-07-25 15:22:47 +0200

    no message

--- MazeRunner2k17/Assets/Scripts/ObjectActivator.cs
+++ MazeRunner2k17/Assets/Scripts/ObjectActivator.cs
@@ -8,11 +8,11 @@ public class ObjectActivator : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        for (int i = 0; 3 > i; i++)
+        /*for (int i = 0; 3 > i; i++)
         {
             spawnDeciders[i] = Random.Range(7 * i, 7 * (i + 1));
             spawnObjective("Object_" + spawnDeciders[i]);
-        }
+        }*/
 	}
 	
 	// Update is called once per frame
