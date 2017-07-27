4664cab7fec6dbf2241e1953ea455bab603a3b8f
Programmering <johan.thomsen@hotmail.com>
2017-07-27 18:53:44 +0200

    Screens opdatering

--- MazeRunner2k17/Assets/Scripts/LanternScript.cs
+++ MazeRunner2k17/Assets/Scripts/LanternScript.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class LanternScript : MonoBehaviour {
 
     public Animator swing;
+    public bool isMoving = false;
+    public float crossfadeTime = 0.5f;
 
 	// Use this for initialization
 	void Start () {
@@ -12,6 +14,13 @@ public class LanternScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+	    if(isMoving = false && (Input.GetAxis("Vertical") != 0 || Input.GetAxis("Horizontal") != 0))
+        {
+            swing.CrossFade("Swinging", crossfadeTime);
+        }
+        else if(isMoving = true && Input.GetAxis("Vertical") == 0 && Input.GetAxis("Horizontal") == 0)
+        {
+            swing.CrossFade("Still", crossfadeTime);
+        }
 	}
 }
